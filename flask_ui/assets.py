38367e790cd9030e28ed168a5dc925849fa111d3
Felipe Chagas <felipechagas@google.com>
2024-08-08 17:04:59 -0300

    Commit

--- flask_ui/assets.py
+++ flask_ui/assets.py
@@ -6,7 +6,7 @@ html_content = '''<!doctype html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@350&display=swap" rel="stylesheet">
     <script src="https://cdn.plot.ly/plotly-latest.min.js" type="text/javascript"></script>
-    <title>Arauco - Genai4SAP</title>
+    <title>Arauco - GenAI4SAP</title>
     <script type="module" crossorigin src="/assets/index-2dc047a4.js"></script>
     <link rel="stylesheet" href="/assets/index-a3ae634d.css">
   </head>
